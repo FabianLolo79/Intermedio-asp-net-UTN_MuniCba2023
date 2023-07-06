1a77d76ed14867d8be1f8760ab13082989a26522
Fabián Enrique García <fabianegl79@gmail.com>
2023-07-06 16:23:52 -0300

    clase4

---
+++ WebApplicationHolaMundoMVC/Models/ClienteViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace WebApplicationHolaMundoMVC.Models
+{
+    public class ClienteViewModel
+    {
+
+        private string nombre;
+        private string apellido;
+
+        public string Nombre { get => nombre; set => nombre=value; }
+
+        public string Apellido { get => apellido; set => apellido=value; }
+    }
+}
